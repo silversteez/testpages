8b8f0aa451eeaa132e9e6e5b5a6464e0b6f93cf9
silversteez <welcomematt@gmail.com>
2014-03-09 20:32:43 -0700

    More firebase test code

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-var myRootRef = new Firebase('https://resplendent-fire-5710.firebaseio.com/');
-myRootRef.set('Hello World!');
-console.log('its working', myRootRef);
+$(function() {
+
+  var myRootRef = new Firebase('https://resplendent-fire-5710.firebaseio.com/');
+  var myBlurbsRef = myRootRef.child('blurbs');
+  console.log('its working', myRootRef);
+
+  var $list = $('#my-list');
+
+  myBlurbsRef.on('child_added', function(snapshot) {
+    var $li = $('<li>');
+    console.log(snapshot.val());
+    $li.text(snapshot.val().blurb);
+    $list.append($li);
+  });
+
+  $("#my-input").keypress(function (e) {
+    if (e.keyCode == 13) {
+      var blurb = $(this).val();
+      myBlurbsRef.push({blurb:blurb});
+      $(this).val('');
+    }
+  });
+
+});
