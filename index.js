39b63ddccd3cee32f5265f0339cabdeeb445b382
silversteez <welcomematt@gmail.com>
2014-03-09 21:26:53 -0700

    More testing

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 $(function() {
 
   var myRootRef = new Firebase('https://resplendent-fire-5710.firebaseio.com/');
+  var simpleLogin = new FirebaseSimpleLogin(myRootRef, function(error, user) {
+    if (error) {
+      console.log(error);
+    } else if (user) {
+      // user authenticated with Firebase
+      console.log('full user:', user);
+      console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+//      myRootRef.set(user.id, 'Anonymous' + user.id.substr(0,8));
+    } else {
+      console.log('Log in new guest.');
+      simpleLogin.login('anonymous');
+    }
+  });
+
   var myBlurbsRef = myRootRef.child('blurbs');
-  console.log('its working', myRootRef);
 
   var $list = $('#my-list');
 
   myBlurbsRef.on('child_added', function(snapshot) {
     var $li = $('<li>');
-    console.log(snapshot.val());
     $li.text(snapshot.val().blurb);
     $list.append($li);
   });
 
-  $("#my-input").keypress(function (e) {
+  $("#my-input").keypress(function(e) {
     if (e.keyCode == 13) {
       var blurb = $(this).val();
       myBlurbsRef.push({blurb:blurb});
